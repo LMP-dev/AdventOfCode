139528222006fe20c4dbbd9a7e5a6ae9b3a029d3
LMP-dev <Lmartorellester@gmail.com>
2022-12-04 11:35:23 +0100

    solved first puzzle

---
+++ 2022/04/puzzle7.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+INPUT_FILE = Path(__file__).parent / "input.txt"
+
+
+def parse_line(line: str) -> tuple[set[int]]:
+    def transform_to_set(assignment: str) -> set[int]:
+        start, stop = assignment.split("-")
+        return set(range(int(start), int(stop) + 1))
+
+    assignment_1, assignment_2 = line.split(",")
+    return transform_to_set(assignment_1), transform_to_set(assignment_2)
+
+
+def main() -> None:
+    contained_pairs = 0
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
+            # Parse line
+            elf_bob, elf_chad = parse_line(line)
+            # Compare sets
+            if elf_bob.issubset(elf_chad) or elf_bob.issuperset(elf_chad):
+                contained_pairs += 1
+
+    print(contained_pairs)
+
+
+if __name__ == "__main__":
+    main()
