c77046fd24bb4e6a000026a836fb0377ab72252d
LMP-dev <Lmartorellester@gmail.com>
2022-12-19 07:37:47 +0100

    small refactoring

--- 2022/16/puzzle31.py
+++ 2022/16/puzzle31.py
@@ -9,29 +9,6 @@ INPUT_FILE = Path(__file__).parent / "input.txt"
 # INPUT_FILE = Path(__file__).parent / "test_input.txt"
 
 
-@cache  # to use cache the function need hashable arguments!
-def solve(position: str, time: int, opened: frozenset) -> int:
-    """Finds the maximum pressure"""
-    # Stop recurrence
-    if time == 0:
-        return 0
-
-    # Solve for moving to next valves
-    pressure = max(solve(neigh, time - 1, opened) for neigh in neighbours[position])
-
-    # Only check for positive flow_rates and not opened valves
-    if flow_rates[position] > 0 and position not in opened:
-        unfrozen_opened = set(opened)
-        unfrozen_opened.add(position)
-        # Open if more pressure than advancing
-        pressure = max(
-            pressure,
-            (time - 1) * flow_rates[position]
-            + solve(position, time - 1, frozenset(unfrozen_opened)),
-        )
-    return pressure
-
-
 def read_valves(file_path: Path) -> tuple[dict]:
     """Returns a dictionary with each valve flow rate and connections"""
     flow_rates = dict()
@@ -49,7 +26,35 @@ def read_valves(file_path: Path) -> tuple[dict]:
     return flow_rates, neighbours
 
 
-flow_rates, neighbours = read_valves(INPUT_FILE)
-total_pressure = solve("AA", 30, frozenset())
-print(total_pressure)
-print(f"--- {(time.time() - start) * 1000} ms ---")
+def main():
+    @cache  # to use cache the function need hashable arguments!
+    def solve(position: str, time: int, opened: frozenset) -> int:
+        """Finds the maximum pressure"""
+        # Stop recurrence
+        if time == 0:
+            return 0
+
+        # Solve for moving to next valves
+        pressure = max(solve(neigh, time - 1, opened) for neigh in neighbours[position])
+
+        # Only check for positive flow_rates and not opened valves
+        if flow_rates[position] > 0 and position not in opened:
+            unfrozen_opened = set(opened)
+            unfrozen_opened.add(position)
+            # Open if more pressure than advancing
+            pressure = max(
+                pressure,
+                (time - 1) * flow_rates[position]
+                + solve(position, time - 1, frozenset(unfrozen_opened)),
+            )
+        return pressure
+
+    flow_rates, neighbours = read_valves(INPUT_FILE)
+    total_pressure = solve("AA", 30, frozenset())
+    print(total_pressure)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    print(f"--- {time.time() - start * 1000} ms ---")
--- 2022/16/puzzle32.py
+++ 2022/16/puzzle32.py
@@ -8,38 +8,6 @@ start = time.time()
 INPUT_FILE = Path(__file__).parent / "input.txt"
 # INPUT_FILE = Path(__file__).parent / "test_input.txt"
 
-# As all solutions are being chequed, intead of paralel, the elephant will be in series:
-@cache  # to use cache the function need hashable arguments!
-def solve(
-    position: str, time: int, opened: frozenset, elephnat_not_started: bool
-) -> int:
-    """Finds the maximum pressure"""
-    # Stop recurrence
-    if time == 0:
-        if elephnat_not_started:
-            return solve("AA", 26, opened, False)
-        return 0
-
-    # Solve for moving to next valves
-    pressure = max(
-        solve(neigh, time - 1, opened, elephnat_not_started)
-        for neigh in neighbours[position]
-    )
-
-    # Only check for positive flow_rates and not opened valves
-    if flow_rates[position] > 0 and position not in opened:
-        unfrozen_opened = set(opened)
-        unfrozen_opened.add(position)
-        # Open if more pressure than advancing
-        pressure = max(
-            pressure,
-            (time - 1) * flow_rates[position]
-            + solve(
-                position, time - 1, frozenset(unfrozen_opened), elephnat_not_started
-            ),
-        )
-    return pressure
-
 
 def read_valves(file_path: Path) -> tuple[dict]:
     """Returns a dictionary with each valve flow rate and connections"""
@@ -58,7 +26,46 @@ def read_valves(file_path: Path) -> tuple[dict]:
     return flow_rates, neighbours
 
 
-flow_rates, neighbours = read_valves(INPUT_FILE)
-total_pressure = solve("AA", 26, frozenset(), True)
-print(total_pressure)
-print(f"--- {(time.time() - start) * 1000} ms ---")
+def main():
+
+    # As all solutions are being chequed, intead of paralel, the elephant will be in series:
+    @cache  # to use cache the function need hashable arguments!
+    def solve(
+        position: str, time: int, opened: frozenset, elephnat_not_started: bool
+    ) -> int:
+        """Finds the maximum pressure"""
+        # Stop recurrence
+        if time == 0:
+            if elephnat_not_started:
+                return solve("AA", 26, opened, False)
+            return 0
+
+        # Solve for moving to next valves
+        pressure = max(
+            solve(neigh, time - 1, opened, elephnat_not_started)
+            for neigh in neighbours[position]
+        )
+
+        # Only check for positive flow_rates and not opened valves
+        if flow_rates[position] > 0 and position not in opened:
+            unfrozen_opened = set(opened)
+            unfrozen_opened.add(position)
+            # Open if more pressure than advancing
+            pressure = max(
+                pressure,
+                (time - 1) * flow_rates[position]
+                + solve(
+                    position, time - 1, frozenset(unfrozen_opened), elephnat_not_started
+                ),
+            )
+        return pressure
+
+    flow_rates, neighbours = read_valves(INPUT_FILE)
+    total_pressure = solve("AA", 26, frozenset(), True)
+    print(total_pressure)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    print(f"--- {time.time() - start * 1000} ms ---")
