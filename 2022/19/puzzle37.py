dd60f70e116df405adf63d0341f3a8371dc0584f
LMP-dev <Lmartorellester@gmail.com>
2023-01-02 14:22:54 +0100

    inputs and part 1 skeleton file

---
+++ 2022/19/puzzle37.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import time
+
+# INPUT_FILE = Path(__file__).parent / "input.txt"
+INPUT_FILE = Path(__file__).parent / "test_input.txt"
+
+
+def parse_input(file_path: Path):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    print(f"--- {(time.time() - start) * 1000} ms ---")
