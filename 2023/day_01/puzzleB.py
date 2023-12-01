f892ea27a4e64884607bba6ad68456750c56919f
LMP-dev <Lmartorellester@gmail.com>
2023-12-01 18:25:18 +0100

    done algortihm but gives wrong solution

--- 2023/day_01/puzzleB.py
+++ 2023/day_01/puzzleB.py
@@ -12,21 +12,94 @@ def parse_input(file_path: Path) -> list[str]:
     return lines
 
 
+def is_it_a_number(character: str) -> tuple[bool, int | str]:
+    try:
+        number = int(character)
+    except ValueError:
+        return (False, character)
+    else:
+        return (True, number)
+
+
+WRITTEN_DIGIT = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+REVERSED_WRITTEN_DIGIT = [word[::-1] for word in WRITTEN_DIGIT]
+DIGITS_MAPPING = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
 def solve_02(data: list[str]) -> int:
     calibration_values = []
-    for i, line in enumerate(data):
-        first = None
-        last = None
-        pattern_end = r"(two).*$"
-        match = re.search(pattern_end, line)
-        if match:
-            print(f"Found two in line  {i}: {match.group(1)}")
+    for line in data:
+        first = None  # left to right
+        last = None  # right to left
+        partial_word = ""
+        # Left to right loop
+        for char in line:
+            # Check digit
+            is_number, val = is_it_a_number(char)
+            if is_number:
+                # Digit found, stop loop
+                first = val
+                break
+            # Check written digits
+            partial_word += char
+            if partial_word in WRITTEN_DIGIT:
+                # Found digit in written form, store and stop loop
+                first = DIGITS_MAPPING[partial_word]
+                break
+            else:
+                found_partial_match = False
+                for word in WRITTEN_DIGIT:
+                    if word.startswith(partial_word):
+                        found_partial_match = True
+                        break  # stops loop from one to nine check
+                if not found_partial_match:
+                    # Reset partial word
+                    partial_word = char
+
+        # Right to left loop
+        partial_word = ""  # reset variable
+        for char in reversed(line):
+            # Check digit
+            is_number, val = is_it_a_number(char)
+            if is_number:
+                # Digit found, stop loop
+                last = val
+                break
+            # Check written digits
+            partial_word += char
+            if partial_word in REVERSED_WRITTEN_DIGIT:
+                # Found digit in written form, store and stop loop
+                last = DIGITS_MAPPING[partial_word[::-1]]
+                break
+            else:
+                found_partial_match = False
+                for word in REVERSED_WRITTEN_DIGIT:
+                    if word.startswith(partial_word):
+                        found_partial_match = True
+                        break  # stops loop from one to nine check
+                if not found_partial_match:
+                    # Reset partial word
+                    partial_word = char
+
+        calibration_values.append(10 * first + last)
+    return sum(calibration_values)
 
 
 def main() -> None:
-    data = parse_input(TEST_INPUT_FILE)
+    data = parse_input(INPUT_FILE)
     solution = solve_02(data)
-    print(solution)
+    print(solution)  # 54140 (too high)
+    # lines 3 (one less) and 7 wrong (one more)
 
 
 if __name__ == "__main__":
