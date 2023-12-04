2221db7afe2fc303d629594c5438ed2617d55b06
lluis.martorell <Lmartorellester@gmail.com>
2023-12-04 08:22:38 +0100

    improved velocity code

--- 2023/day_04/puzzleB.py
+++ 2023/day_04/puzzleB.py
@@ -1,11 +1,12 @@
 from pathlib import Path
+from functools import lru_cache
 
 import numpy as np
 
 INPUT_FILE_PATH = Path(__file__).parent
 
 
-def parse_input(file_path: Path) -> dict[tuple[list[int], list[int]]]:
+def parse_input(file_path: Path) -> list[tuple[tuple[int, ...], tuple[int, ...]]]:
     # Utility function
     def convert_to_int_list(string_list: list[str]) -> list[int]:
         number_list = []
@@ -29,24 +30,31 @@ def parse_input(file_path: Path) -> dict[tuple[list[int], list[int]]]:
                 winning_numbers_section.strip().split(" ")
             )
             own_numbers = convert_to_int_list(own_numbers_section.strip().split(" "))
-            scratch_cards.append((winning_numbers, own_numbers))
+            scratch_cards.append((tuple(winning_numbers), tuple(own_numbers)))
     return scratch_cards
 
 
-def solve_01(data: dict[tuple[list[int], list[int]]]) -> int:
+def solve_01(data: list[tuple[tuple[int, ...], tuple[int, ...]]]) -> int:
     # variables initialization
     scratch_cards_count = np.ones(len(data))  # stores nº of cards and copies
 
     for id, (winning, own) in enumerate(data):
-        for _ in range(int(scratch_cards_count[id])):
-            matches = 0
-            for number in own:
-                if number in winning:
-                    matches += 1
-            # Create copies
-            for i in range(matches):
-                scratch_cards_count[id + i + 1] = scratch_cards_count[id + i + 1] + 1
+        matches = find_matches(winning, own)
+        # Create copies
+        for i in range(matches):
+            scratch_cards_count[id + i + 1] = (
+                scratch_cards_count[id + i + 1] + 1 * scratch_cards_count[id]
+            )
     return np.sum(scratch_cards_count)
+
+
+@lru_cache
+def find_matches(first_list: tuple[int], second_list: tuple[int]) -> int:
+    matches = 0
+    for number in second_list:
+        if number in first_list:
+            matches += 1
+    return matches
 
 
 def main() -> None:
@@ -55,7 +63,7 @@ def main() -> None:
     solution = solve_01(data)
     print(f"The solution of the example is {solution}")
     data = parse_input(INPUT_FILE_PATH / "input.txt")
-    solution = solve_01(data)
+    solution = solve_01(data)  # 14427616
     print(f"The solution of part 1 is {solution}")
 
 
