fd22dbcaf3cd81cd6730b3a1cc3074f588f56093
LMP-dev <Lmartorellester@gmail.com>
2023-12-06 12:00:11 +0100

    wip, error parsing

--- 2023/day_06/puzzleA .py
+++ 2023/day_06/puzzleA .py
@@ -14,18 +14,29 @@ def read_data(
 
 
 def parse_input(file_content: list[str]) -> list[tuple[int, int]]:
-    return
+    _, times = file_content[0].split(":")
+    _, distances = file_content[1].split(":")
+    raw_times_list = times.strip().split(" ")
+    raw_distances_list = distances.strip().split(" ")
+    times_list = []
+    distances_list = 
+    return [(int(time), int(dist)) for time, dist in zip(times_list, distances_list)]
 
 
-def solve_01(data: list[int]) -> int:
-    seeds, finder = data
-    # variables initialization
-    minimum_location = math.inf
-    for seed in seeds:
-        location = finder.get_location(seed)
-        minimum_location = min(minimum_location, location)
-
-    return minimum_location
+def solve_01(data: list[tuple[int, int]]) -> int:
+    number_ways_to_win = []
+    product = 1
+    for race in data:
+        ways_to_win = 0
+        time, max_dist = race
+        for hold_time in range(time):
+            time_to_race = time - hold_time
+            distance = hold_time * time_to_race
+            if distance > max_dist:
+                ways_to_win += 1
+        number_ways_to_win.append(ways_to_win)
+        product *= ways_to_win
+    return product
 
 
 def main() -> None:
@@ -35,7 +46,7 @@ def main() -> None:
     solution = solve_01(data)
     print(f"The solution of the example is {solution}")
     data = parse_input(INPUT_FILE_PATH / "input.txt")
-    solution = solve_01(data)  # 25004
+    solution = solve_01(data)
     print(f"The solution of part 1 is {solution}")
 
 
