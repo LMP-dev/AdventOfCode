d6e61051d5f5f5840c324497a5b667af609bfcc5
LMP-dev <Lmartorellester@gmail.com>
2023-12-13 18:48:18 +0100

    wip for part 1

--- 2023/day_10/puzzleA .py
+++ 2023/day_10/puzzleA .py
@@ -1,10 +1,22 @@
 # Standard library
 from pathlib import Path
+from dataclasses import dataclass
+from enum import Enum, auto
 
-# 3rd party library
+# 3rd party libraries
 import numpy as np
 
 INPUT_FILE_PATH = Path(__file__).parent
+
+class Direction(Enum):
+    NORD = auto()
+    EAST = auto()
+    SUD = auto()
+    WEST = auto()
+
+@dataclass
+class Move:
+
 
 
 def read_data(
@@ -15,14 +27,36 @@ def read_data(
     return lines
 
 
-def parse_input(file_content: list[str]) -> tuple[list[list[str]], tuple[int,int]]:
+def parse_input(file_content: list[str]) -> tuple[list[list[str]], tuple[int, int]]:
     matrix = []
-    start_position = (,)
+    for r, grid_line in enumerate(file_content):
+        new_row = []
+        for c, char in enumerate(grid_line):
+            new_row.append(char)
+            if char == "S":
+                start_position = (r, c)
+        matrix.append(new_row)
     return matrix, start_position
 
 
-def solve_01(data: tuple[list[list[str]], tuple[int,int]]) -> int:
+def solve_01(data: tuple[list[list[str]], tuple[int, int]]) -> int:
+    # Unpack data
+    grid, start_position = data
     step = 0
+    st = np.array(start_position)
+
+    next_step_moves = [()]
+    # nord_route = {"active": True, "path": []}
+    # east_route = {"active": True, "path": []}
+    # south_route = {"active": True, "path": []}
+    # west_route = {"active": True, "path": []}
+
+
+    positions = [data[1]]
+    visited_positions = []
+
+    while True:
+
 
     return step
 
