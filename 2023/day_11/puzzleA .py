67a043d3ac8ad9e9dae9fa0a7e79d74386e3f532
LMP-dev <Lmartorellester@gmail.com>
2023-12-20 08:26:18 +0100

    set-up part 1

---
+++ 2023/day_11/puzzleA .py
@@ -0,0 +1,44 @@
+# Standard library
+from pathlib import Path
+
+INPUT_FILE_PATH = Path(__file__).parent
+
+
+def read_data(
+    file_path: Path,
+) -> list[str]:
+    with open(file_path) as file:
+        lines = [line.strip() for line in file]
+    return lines
+
+
+def parse_input(file_content: list[str]):
+    grid = []
+
+    return grid
+
+
+def solve_01(data: tuple[list[list[str]], tuple[int, int]]) -> int:
+    step = 0
+
+    return step
+
+
+def main() -> None:
+    # input.txt | example_1.txt | example_1_v2.txt
+    file_content = read_data(INPUT_FILE_PATH / "example_1.txt")
+    data = parse_input(file_content)
+    solution = solve_01(data)
+    print(f"The solution of the example 1 is {solution}")
+    file_content = read_data(INPUT_FILE_PATH / "example_2.txt")
+    data = parse_input(file_content)
+    solution = solve_01(data)
+    print(f"The solution of the example 2 is {solution}")
+    file_content = read_data(INPUT_FILE_PATH / "input.txt")
+    data = parse_input(file_content)
+    solution = solve_01(data)
+    print(f"The solution of the part 1 is {solution}")
+
+
+if __name__ == "__main__":
+    main()
