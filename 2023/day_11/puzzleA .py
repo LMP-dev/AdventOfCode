0004fd8719ed7b90fda6bb1543a4095fbc8c49ed
LMP-dev <Lmartorellester@gmail.com>
2023-12-20 18:11:44 +0100

    solved part 1

--- 2023/day_11/puzzleA .py
+++ 2023/day_11/puzzleA .py
@@ -22,18 +22,56 @@ def parse_input(file_content: list[str]) -> list[list[str]]:
         grid.append(row)
 
     # Expand columns
-    ...
+    indexes_to_exapnd = []
+    for i, _ in enumerate(line):
+        column = [row[i] for row in grid]
+        if column.count(".") == len(column):
+            indexes_to_exapnd.append(i)
+    accumulated_index = 0
+    for index in indexes_to_exapnd:
+        for row in grid:
+            row.insert(index + accumulated_index, ".")
+        accumulated_index += 1
 
-    return
+    return grid
 
 
-def solve_01(data: tuple[list[list[str]], tuple[int, int]]) -> int:
-    step = 0
-    # create all pairs
+def find_galaxies_positions(universe: list[list[str]]) -> list[tuple[int, int]]:
+    galaxies: list[tuple[int, int]] = []
+    for r, row in enumerate(universe):
+        for c, item in enumerate(row):
+            if item == "#":
+                galaxies.append((r, c))
+    return galaxies
 
-    # find distances by coordinates
 
-    return step
+def create_galaxy_pairs(
+    galaxies: list[tuple[int, int]]
+) -> list[tuple[tuple[int, int], tuple[int, int]]]:
+    pairs = []
+    while len(galaxies) != 1:
+        galaxy = galaxies.pop()
+        for other_galaxy in galaxies:
+            pairs.append((galaxy, other_galaxy))
+    return pairs
+
+
+def find_distance_between_galaxies(
+    origin: tuple[int, int], dest: tuple[int, int]
+) -> int:
+    v_dist = abs(dest[0] - origin[0])
+    h_dist = abs(dest[1] - origin[1])
+    return v_dist + h_dist
+
+
+def solve_01(data: list[list[str]]) -> int:
+    paths_sum = 0
+    galaxies_coordinates = find_galaxies_positions(data)
+    galaxy_pairs = create_galaxy_pairs(galaxies_coordinates)
+    for pair in galaxy_pairs:
+        path_lenght = find_distance_between_galaxies(pair[0], pair[1])
+        paths_sum += path_lenght
+    return paths_sum
 
 
 def main() -> None:
