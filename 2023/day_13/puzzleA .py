cd241890180608cb3ae97e75d2623ec4b2a2a5e9
LMP-dev <Lmartorellester@gmail.com>
2023-12-21 07:42:28 +0100

    parsing done

--- 2023/day_13/puzzleA .py
+++ 2023/day_13/puzzleA .py
@@ -12,11 +12,20 @@ def read_data(
     return lines
 
 
-def parse_input(file_content: list[str]):
-    return
+def parse_input(file_content: list[str]) -> list[list[list[str]]]:
+    patterns = []
+    pattern = []
+    for line in file_content:
+        if line:
+            pattern.append(list(line))
+        else:
+            patterns.append(pattern)
+            pattern = list()
+    patterns.append(pattern)
+    return patterns
 
 
-def solve_01(data) -> int:
+def solve_01(data: list[list[list[str]]]) -> int:
     return
 
 
@@ -26,10 +35,10 @@ def main() -> None:
     data = parse_input(file_content)
     solution = solve_01(data)
     print(f"The solution of the example 1 is {solution}")
-    file_content = read_data(INPUT_FILE_PATH / "input.txt")
-    data = parse_input(file_content)
-    solution = solve_01(data)
-    print(f"The solution of the part 1 is {solution}")
+    # file_content = read_data(INPUT_FILE_PATH / "input.txt")
+    # data = parse_input(file_content)
+    # solution = solve_01(data)
+    # print(f"The solution of the part 1 is {solution}")
 
 
 if __name__ == "__main__":
