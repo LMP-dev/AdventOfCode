57dc0344ab770ca21dff932f7decb73e4073129d
lluis.martorell <Lmartorellester@idiada.com>
2023-12-23 13:25:53 +0100

    WIP: creating operations for cycles

--- 2023/day_14/operations.py
+++ 2023/day_14/operations.py
@@ -9,3 +9,32 @@ def create_sections(
     sections.append((ini, max_value + 1))
 
     return sections
+
+
+def classify_between_sections(
+    sections: list[tuple[int, int]], all_values: list[int]
+) -> dict[tuple[int, int], list[int]]:
+    filled_sections = {}
+    for section in sections:
+        values = [val for val in all_values if val > section[0] and val < section[1]]
+        filled_sections.update({section: values})
+
+    return filled_sections
+
+
+def reorder_section_values(
+    section: tuple[int, int], values: list[int], inverse: bool = False
+) -> list[int]:
+    new_values = []
+    if not inverse:
+        occupied_pos = section[0]
+        for _ in values:
+            occupied_pos += 1  # Next position to be occupied
+            new_values.append(occupied_pos)
+    else:
+        occupied_pos = section[1]
+        for _ in values:
+            occupied_pos -= 1  # Next position to be occupied
+            new_values.append(occupied_pos)
+
+    return new_values
