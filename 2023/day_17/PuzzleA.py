83b6def0a68b2ed1752da09b6429d7b90884717c
LMP-dev <Lmartorellester@gmail.com>
2023-12-27 00:38:55 +0100

    solved part 1

--- 2023/day_17/PuzzleA.py
+++ 2023/day_17/PuzzleA.py
@@ -1,11 +1,44 @@
 # Standard library
 from pathlib import Path
+from heapq import heappop, heappush
+from dataclasses import dataclass
+from enum import Enum, auto
 
 # .py modules
-from city_block import CityBlock, Direction
-import distance
 
 INPUT_FILE_PATH = Path(__file__).parent
+
+
+class Direction(Enum):
+    RIGHT = auto()
+    UP = auto()
+    LEFT = auto()
+    DOWN = auto()
+
+
+OPPOSITE_DIRECTION = {
+    Direction.RIGHT: Direction.LEFT,
+    Direction.UP: Direction.DOWN,
+    Direction.LEFT: Direction.RIGHT,
+    Direction.DOWN: Direction.UP,
+}
+
+NEXT_STEP_OFFSETS: dict[Direction, tuple[int, int]] = {
+    Direction.RIGHT: (0, 1),
+    Direction.UP: (-1, 0),
+    Direction.LEFT: (0, -1),
+    Direction.DOWN: (1, 0),
+}
+
+
+@dataclass(frozen=True)
+class State:
+    location: tuple[int, int]
+    facing: Direction
+
+    def __lt__(self, other) -> bool:
+        if isinstance(other, State):
+            return self.location < other.location
 
 
 def read_data(
@@ -28,37 +61,52 @@ def parse_input(file_content: list[str]) -> Grid:
     return grid
 
 
+def add_tuples(one: tuple[int, int], other: tuple[int, int]) -> tuple[int, int]:
+    return (one[0] + other[0], one[1] + other[1])
+
+
 def solve_01(data) -> int:
-    queue: list[CityBlock] = [CityBlock((0, 0), Direction.DOWN, 0)]
-    visited: set[tuple(int, int)] = set()
+    queue: list[tuple[int, State, int]] = [
+        (0, State((0, 0), Direction.DOWN), 1),
+        (0, State((0, 0), Direction.RIGHT), 1),
+    ]
+    visited: set[tuple[State, int]] = set()
 
     finishing_loc = max(data.keys())
     # distance_funct = distance.get_distance_a_star(finishing_loc)
 
     while queue:
         # Sort according to heat loss
-        queue.sort(key=distance.distance_dikstra)
+        heat, position, num_steps = heappop(queue)
 
-        lowest_block = queue.pop(0)
-        # try:
-        #     if lowest_block.accumulated_heat_loss == queue[0].accumulated_heat_loss:
-        #         print(
-        #             f"There was more blocks with same min distance!!!\nBlock: {lowest_block} and {queue[0]}"
-        #         )
-        # except IndexError:
-        #     pass
-
-        if lowest_block.loc in visited:
+        if (position, num_steps) in visited:
             continue
-        visited.add(lowest_block.loc)
+        visited.add((position, num_steps))
 
-        if lowest_block.loc == finishing_loc:
-            break
-        else:
-            queue.extend(lowest_block.next_blocks(data))
+        if position.location == finishing_loc:
+            return heat
 
-    print(lowest_block.heat_loss_path)
-    return lowest_block.accumulated_heat_loss
+        # Find allowed new directions
+        next_directions: list[Direction] = [
+            Direction.RIGHT,
+            Direction.UP,
+            Direction.LEFT,
+            Direction.DOWN,
+        ]
+        next_directions.remove(OPPOSITE_DIRECTION[position.facing])
+        for direction in next_directions:
+            if direction == position.facing:
+                next_num_steps = num_steps + 1
+                if next_num_steps > 3:
+                    continue
+            else:
+                next_num_steps = 1
+            offset = NEXT_STEP_OFFSETS[direction]
+            loc = add_tuples(position.location, offset)
+            if loc not in data:
+                continue
+            new_heat = data[loc]
+            heappush(queue, (heat + new_heat, State(loc, direction), next_num_steps))
 
 
 def main() -> None:
@@ -67,10 +115,10 @@ def main() -> None:
     data = parse_input(file_content)
     solution = solve_01(data)
     print(f"The solution of the example 1 is {solution}")
-    # file_content = read_data(INPUT_FILE_PATH / "input.txt")
-    # data = parse_input(file_content)
-    # solution = solve_01(data)
-    # print(f"The solution of the part 1 is {solution}")
+    file_content = read_data(INPUT_FILE_PATH / "input.txt")
+    data = parse_input(file_content)
+    solution = solve_01(data)
+    print(f"The solution of the part 1 is {solution}")
 
 
 if __name__ == "__main__":
