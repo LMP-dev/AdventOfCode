81b4998110adfa3f009584fb758080190c15a13c
LMP-dev <Lmartorellester@gmail.com>
2023-12-31 17:43:50 +0100

    WIP_ created graph corners and normalized graph corners using only tuples

--- 2023/day_18/PuzzleB.py
+++ 2023/day_18/PuzzleB.py
@@ -5,31 +5,13 @@ from dataclasses import dataclass
 
 INPUT_FILE_PATH = Path(__file__).parent
 
-
+STARTING_POS = (0, 0)
 MOVEMENT: dict[str, tuple[int, int]] = {
     "R": (0, 1),
     "U": (-1, 0),
     "L": (0, -1),
     "D": (1, 0),
 }
-
-
-@dataclass(frozen=True)
-class TrenchCorner:
-    pos: tuple(int, int)
-
-    def normalize(self, distance: tuple[int, int]) -> None:
-        self.pos = add_tuples(self.pos, distance)
-
-    def __eq__(self, other: TrenchCorner | tuple[int, int]) -> bool:
-        if isinstance(other, TrenchCorner):
-            return self.pos == other.pos
-        elif isinstance(other, tuple):
-            return self.pos == other
-        else:
-            raise Exception(
-                f"TrenchCorner class can only be compared to itself or tuples[int,int]. Comparison tryied with {type(other)}"
-            )
 
 
 def read_data(
@@ -64,18 +46,6 @@ def add_tuples(one: tuple[int, int], other: tuple[int, int]) -> tuple[int, int]:
     return (one[0] + other[0], one[1] + other[1])
 
 
-def find_min_coordinates(
-    current_min_coords: tuple[int, int], new_trenches: list[tuple[int, int]]
-) -> tuple[int, int]:
-    min_row_coords = [current_min_coords[0]].extend(
-        [trench[0] for trench in new_trenches]
-    )
-    min_col_coords = [current_min_coords[1]].extend(
-        [trench[1] for trench in new_trenches]
-    )
-    return min(min_row_coords), min(min_col_coords)
-
-
 def advance_one_direction(
     position: tuple[int, int], direction: str, steps: int
 ) -> tuple[int, int]:
@@ -88,44 +58,67 @@ def generate_digged_trench_corners(
     starting_pos: tuple[int, int], instructions: list[tuple[str, int]]
 ) -> dict[tuple[int, int], list[tuple[int, int]]]:
     # Initialize variables
-    initial_corner = TrenchCorner(starting_pos)
-    current_corner = initial_corner
-    loop_corners: dict[tuple[int, int], list[tuple[int, int]]] = {initial_corner: []}
+    current_corner = starting_pos
+    loop_corners: dict[tuple[int, int], list[tuple[int, int]]] = {current_corner: []}
     # Populate corners graph
     for instruction in instructions:
         direction, steps = instruction
-        new_pos = advance_one_direction(current_corner.pos, direction, steps)
-        new_corner = TrenchCorner(new_pos)
+        new_corner = advance_one_direction(current_corner, direction, steps)
         loop_corners[current_corner].append(new_corner)
-        if new_pos != starting_pos:
+        if new_corner != starting_pos:
             loop_corners.update({new_corner: [current_corner]})
         else:  # Special case end of loop
-            loop_corners[initial_corner].append(current_corner)
+            loop_corners[starting_pos].append(current_corner)
         current_corner = new_corner
     return loop_corners
 
 
-def find_min_coordinates(trenches: list[tuple[int, int]]) -> tuple[int, int]:
-    ...
-    return
+def find_min_max_coordinates(
+    trenches: list[tuple[int, int]],
+) -> tuple[tuple[int, int], tuple[int, int]]:
+    min_row = STARTING_POS[0]
+    min_col = STARTING_POS[1]
+    max_row = STARTING_POS[0]
+    max_col = STARTING_POS[1]
+    for trench in trenches:
+        min_row = min(min_row, trench[0])
+        min_col = min(min_col, trench[1])
+        max_row = max(max_row, trench[0])
+        max_col = max(max_col, trench[1])
+    return (min_row, min_col), (max_row, max_col)
 
 
-def normalize_trenches_corners_coordinates(
-    trenches: dict[tuple[int, int], list[tuple[int, int]]]
+def normalize_list_coordinates(
+    coords_list: list[tuple[int, int]], vector: tuple[int, int]
+) -> list[tuple[int, int]]:
+    return [add_tuples(coord, vector) for coord in coords_list]
+
+
+def normalize_graph_coordinates(
+    graph: dict[tuple[int, int], list[tuple[int, int]]], vector: tuple[int, int]
 ) -> dict[tuple[int, int], list[tuple[int, int]]]:
-    ...
-    return
+    return {
+        add_tuples(k, vector): normalize_list_coordinates(v, vector)
+        for k, v in graph.items()
+    }
 
 
 def solve_02(data: list[tuple[str, int]]) -> int:
     # Follow instructions to generate all corners coordinates
-    loop_trenches = generate_digged_trench_corners((0, 0), data)
+    corners_graph = generate_digged_trench_corners(STARTING_POS, data)
 
-    # Normalise coordinates to know the initial rectangle
+    # Normalise coordinates to have (0,0) in the top-left corner
+    corners_list = corners_graph.keys()
+    min_coordinates, max_coordinates = find_min_max_coordinates(corners_list)
+    vector = (-min_coordinates[0], -min_coordinates[1])
+    normalized_corners_graph = normalize_graph_coordinates(corners_graph, vector)
+    norm_max_coordinates = add_tuples(max_coordinates, vector)
 
     # Remove the area of the non-digged rectangles
+    total_area = (norm_max_coordinates[0] + 1) * (norm_max_coordinates[1] + 1)
+    # Left vertical side
 
-    return
+    return total_area
 
 
 def main() -> None:
