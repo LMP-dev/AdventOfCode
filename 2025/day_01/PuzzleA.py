c315b4d549675b8f5e7bec84b65780d6a69e82ec
LMP-Dev <Lmartorellester@gmail.com>
2025-12-02 19:31:01 +0100

    feat: solved Part 1

--- 2025/day_01/PuzzleA.py
+++ 2025/day_01/PuzzleA.py
@@ -7,35 +7,44 @@ INPUT_FILE_PATH = Path(__file__).parent
 
 
 @dataclass
-class Dial:
-    lower: int = 0  # only works exactly for lower == 0
-    higher: int = 99
+class Dial099:
     current: int = 50
 
     zero_count: int = 0
 
     def find_0s(self, instructions: list[tuple[str, int]]) -> int:
+        print(f"The dial points to {self.current}")
         for direction, number in instructions:
+            ## Remove complete spins
+            spins, rem = divmod(number, 100)  # 0 to 99 is 100 numbers
+
             if direction == "R":
-                self._move_right(number)
-            if direction == "L":
-                self._move_left(number)
+                self._move_right(rem)
+            elif direction == "L":
+                self._move_left(rem)
+            else:
+                raise Exception(f"Incorrect direction (R,L): {direction}")
+
             if self.current == 0:
                 self.zero_count += 1
 
         return self.zero_count
 
     def _move_left(self, positions: int):
-        self.current = self.current - positions
-        spins, rem = divmod(self.current, self.higher)
-        if self.current < self.lower:
+        next_number = self.current - positions
+        if next_number < 0:
+            spins, rem = divmod(next_number, 99)
             self.current = rem + 1  # -1 --> 99 (rem=98)
+        else:
+            self.current = next_number
 
     def _move_right(self, positions: int):
-        self.current = self.current + positions
-        spins, rem = divmod(self.current, self.higher)
-        if rem != 0:
+        next_number = self.current + positions
+        if next_number > 99:
+            spins, rem = divmod(next_number, 99)
             self.current = rem - 1  # 100 --> 0 (rem = 1)
+        else:
+            self.current = next_number
 
 
 def read_data(
@@ -52,7 +61,7 @@ def parse_input(file_content: list[str]) -> list[tuple[str, int]]:
 
 
 def solve_01(data: Any) -> int:
-    dial = Dial()
+    dial = Dial099()
 
     count = dial.find_0s(data)
 
