61701a306a66006c1e10fa62217ae2bf928055ca
LMP-Dev <Lmartorellester@gmail.com>
2025-12-02 21:00:28 +0100

    feat: solved part 1

--- 2025/day_02/PuzzleA.py
+++ 2025/day_02/PuzzleA.py
@@ -1,6 +1,5 @@
 # Standard library
 from pathlib import Path
-from typing import Any
 
 INPUT_FILE_PATH = Path(__file__).parent
 
@@ -13,12 +12,40 @@ def read_data(
     return lines
 
 
-def parse_input(file_content: list[str]) -> Any:
-    return 
+def parse_input(file_content: list[str]) -> list[tuple[int, int]]:
+    input_line = file_content[0]
+    range_list = input_line.split(",")
+
+    return [map(int, item.split("-")) for item in range_list]
 
 
-def solve_01(data: Any) -> int:
-    return
+def is_bad_id(num: int) -> bool:
+    # Convert back to string
+    num_str = str(num)
+
+    half_size, rem = divmod(len(num_str), 2)
+    # Avoid odd combinations of characters
+    if rem != 0:
+        return False
+    first_half = num_str[:half_size]
+    second_half = num_str[half_size:]
+
+    # Check equal half
+    if int(first_half) == int(second_half):
+        return True
+
+    return False
+
+
+def solve_01(data: list[tuple[int, int]]) -> int:
+    invalid_ids: list[int] = []
+
+    for min_id, max_id in data:
+        for id in range(min_id, max_id + 1):  # +1 to end in the number
+            if is_bad_id(id):
+                invalid_ids.append(id)
+
+    return sum(invalid_ids)
 
 
 def main() -> None:
