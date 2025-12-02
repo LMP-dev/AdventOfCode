ce079016a0cdcd47129ae7f3cea0774b5816695d
LMP-Dev <Lmartorellester@gmail.com>
2025-12-02 18:36:15 +0100

    feat: setup day 1 part 1

--- 2025/day_01/PuzzleA.py
+++ 2025/day_01/PuzzleA.py
@@ -1,8 +1,31 @@
 # Standard library
 from pathlib import Path
 from typing import Any
+from dataclasses import dataclass
 
 INPUT_FILE_PATH = Path(__file__).parent
+
+
+@dataclass
+class Dial:
+    lower: int = 0
+    higher: int = 99
+    current: int = 50
+
+    zero_count: int = 0
+
+    def find_0s(self, instructions: list[tuple[str, int]]) -> int:
+        for direction, number in instructions:
+            if direction == "R":
+                self._move_right(number)
+            if direction == "L":
+                self._move_left(number)
+
+        return self.zero_count
+
+    def _move_left(self, positions: int): ...
+
+    def _move_right(self, positions: int): ...
 
 
 def read_data(
@@ -13,11 +36,14 @@ def read_data(
     return lines
 
 
-def parse_input(file_content: list[str]) -> Any:
-    return 
+def parse_input(file_content: list[str]) -> list[tuple[str, int]]:
+    instructions = [(line[0], int(line[1:])) for line in file_content]
+    return instructions
 
 
 def solve_01(data: Any) -> int:
+    dial = Dial()
+
     return
 
 
@@ -27,10 +53,10 @@ def main() -> None:
     data = parse_input(file_content)
     solution = solve_01(data)
     print(f"The solution of the example 1 is {solution}")
-    file_content = read_data(INPUT_FILE_PATH / "input.txt")
-    data = parse_input(file_content)
-    solution = solve_01(data)
-    print(f"The solution of the part 1 is {solution}")
+    # file_content = read_data(INPUT_FILE_PATH / "input.txt")
+    # data = parse_input(file_content)
+    # solution = solve_01(data)
+    # print(f"The solution of the part 1 is {solution}")
 
 
 if __name__ == "__main__":
