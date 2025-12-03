f8ba257c4814a6f99b2df89bbe37b2978f98c5d3
LMP-Dev <Lmartorellester@gmail.com>
2025-12-03 08:41:03 +0100

    feat: solved part 1

--- 2025/day_03/PuzzleA.py
+++ 2025/day_03/PuzzleA.py
@@ -13,12 +13,53 @@ def read_data(
     return lines
 
 
-def parse_input(file_content: list[str]) -> Any:
-    return 
+def parse_input(file_content: list[str]) -> list[list[int]]:
+    battery_banks = []
+    for line in file_content:
+        battery_banks.append([int(c) for c in line])
+    return battery_banks
+
+
+def find_joltage(row_num: list[int]) -> int:
+    first, f_index = None, None
+    second = None
+
+    # First digit loop
+    for i, num in enumerate(row_num[:-1]):  # do not consider last digit
+        if first is None:
+            first = num
+            f_index = i
+        else:
+            if num == 9:
+                first = num
+                f_index = i
+                break  # Stop at first highest possible number
+            elif num > first:
+                first = num
+                f_index = i
+
+    # Second digit loop
+    for num in row_num[f_index + 1 :]:
+        if second is None:
+            second = num
+        else:
+            if num == 9:
+                second = num
+                break  # Stop at first highest possible number
+            elif num > second:
+                second = num
+
+    joltage = first * 10 + second
+    # print(f"The Joltage is {joltage}")
+    return joltage
 
 
 def solve_01(data: Any) -> int:
-    return
+    joltages = []
+    for bank in data:
+        joltages.append(find_joltage(bank))
+
+    return sum(joltages)
 
 
 def main() -> None:
