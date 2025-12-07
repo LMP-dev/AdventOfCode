a45d05e78f9e182716351ba65770c4721a3a86cd
LMP-dev <Lmartorellester@gmail.com>
2025-12-07 14:33:06 +0100

    feat: potential solution problem 1

--- 2025/day_06/PuzzleA.py
+++ 2025/day_06/PuzzleA.py
@@ -13,12 +13,39 @@ def read_data(
     return lines
 
 
-def parse_input(file_content: list[str]) -> Any:
-    return 
+def parse_input(file_content: list[str]) -> tuple[list[list[int], list[str]]]:
+    numbers: list[list[int]] = []
+    operators = None
+
+    # Parse line of numbers and store each line
+    for i, line in enumerate(file_content):
+        if i == len(file_content) - 1:
+            operators = line.split()
+        else:
+            numbers.append(line.split())
+
+    numbers_to_operate: list[list[int]] = []
+    # Create tuples of each numerand
+    for index, _ in enumerate(operators):
+        numbers_to_operate.append([int(nums[index]) for nums in numbers])
+
+    return numbers_to_operate, operators
 
 
-def solve_01(data: Any) -> int:
-    return
+def solve_01(data: tuple[list[list[int], list[str]]]) -> int:
+    numbers_to_operate, operators = data
+    total = 0
+
+    for numbers, operator in zip(numbers_to_operate, operators):
+        if operator == "+":
+            total += sum(numbers)
+        elif operator == "*":
+            result = 1
+            for num in numbers:
+                result *= num
+            total += result
+
+    return total
 
 
 def main() -> None:
@@ -26,11 +53,11 @@ def main() -> None:
     file_content = read_data(INPUT_FILE_PATH / "example_1.txt")
     data = parse_input(file_content)
     solution = solve_01(data)
-    print(f"The solution of the example 1 is {solution}")
+    print(f"The solution of the example 1 is {solution}")  # Solution 4277556
     file_content = read_data(INPUT_FILE_PATH / "input.txt")
     data = parse_input(file_content)
     solution = solve_01(data)
-    print(f"The solution of the part 1 is {solution}")
+    print(f"The solution of the part 1 is {solution}")  # Solution 6417439773370
 
 
 if __name__ == "__main__":
