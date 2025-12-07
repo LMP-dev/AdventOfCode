78c12d5faa00ef456fdf5cdaa64537f076e50ddc
LMP-dev <Lmartorellester@gmail.com>
2025-12-07 14:00:02 +0100

    feat: wip solution part 1

--- 2025/day_05/PuzzleA.py
+++ 2025/day_05/PuzzleA.py
@@ -14,23 +14,60 @@ def read_data(
 
 
 def parse_input(file_content: list[str]) -> Any:
-    return 
+    ordered_ranges: list[tuple[int, int]] = []
+    ingredient_ids: list[int] = []
+
+    split_mode = True
+
+    for line in file_content:
+        if line == "":
+            split_mode = False
+            continue
+        if split_mode:
+            start, end = line.split("-")
+            ordered_ranges.append((int(start), int(end)))
+        else:
+            ingredient_ids.append(int(line))
+    return sorted(ordered_ranges), ingredient_ids
 
 
 def solve_01(data: Any) -> int:
-    return
+    ordered_ranges, ingredients_ids = data
+    count = 0
+
+    start_range = [a for a, b in ordered_ranges]
+    end_range = [b for a, b in ordered_ranges]
+
+    for id in ingredients_ids:
+        for i, start in enumerate(start_range):
+            if id <= start:
+                if id == start:
+                    beginning = start
+                    end = end_range[i]
+                else:
+                    beginning = start_range[i - 1]
+                    end = end_range[i - 1]
+                if id >= beginning and id <= end:
+                    count += 1
+
+        # Check for last range
+        if id > start:  # avoids last id equal to beginning range
+            if id >= start_range[-1] and id <= end_range[-1]:
+                count += 1
+
+    return count
 
 
 def main() -> None:
     # input.txt | example_1.txt
     file_content = read_data(INPUT_FILE_PATH / "example_1.txt")
     data = parse_input(file_content)
-    solution = solve_01(data)
+    solution = solve_01(data)  # Solution 3
     print(f"The solution of the example 1 is {solution}")
     file_content = read_data(INPUT_FILE_PATH / "input.txt")
     data = parse_input(file_content)
     solution = solve_01(data)
-    print(f"The solution of the part 1 is {solution}")
+    print(f"The solution of the part 1 is {solution}")  # solution 775
 
 
 if __name__ == "__main__":
