658dcd31d369403dbdb350403a9954b283f0af06
LMP-dev <Lmartorellester@gmail.com>
2025-12-13 14:14:49 +0100

    refactor: set up problem 1

---
+++ 2025/day_08/PuzzleA.py
@@ -0,0 +1,46 @@
+# Standard library
+from pathlib import Path
+from typing import Any
+
+INPUT_FILE_PATH = Path(__file__).parent
+
+
+def read_data(
+    file_path: Path,
+) -> list[str]:
+    with open(file_path) as file:
+        lines = [line.strip() for line in file]
+    return lines
+
+
+def parse_input(file_content: list[str]) -> Any:
+    return
+
+
+def solve_01(data: Any) -> int:
+    # Calculate all distances between points
+
+    # Sort by distances
+
+    # pop distances and create circuits
+    # any(x in list for list in circuits)  # circuits of 1 apart or not needed?
+
+    # calculate 3 highest circuits
+
+    return
+
+
+def main() -> None:
+    # input.txt | example_1.txt
+    file_content = read_data(INPUT_FILE_PATH / "example_1.txt")
+    data = parse_input(file_content)
+    solution = solve_01(data)
+    print(f"The solution of the example 1 is {solution}")  # Solution 40
+    file_content = read_data(INPUT_FILE_PATH / "input.txt")
+    data = parse_input(file_content)
+    solution = solve_01(data)
+    print(f"The solution of the part 1 is {solution}")
+
+
+if __name__ == "__main__":
+    main()
