e6ab6e9fdbc0843ac0b5af62319dedb7859a8736
LMP-dev <Lmartorellester@gmail.com>
2025-12-14 09:34:21 +0100

    refactor: setup problem 1

---
+++ 2025/day_10/PuzzleA.py
@@ -0,0 +1,37 @@
+# Standard library
+from pathlib import Path
+from typing import Any
+
+INPUT_FILE_PATH = Path(__file__).parent
+
+
+def read_data(
+    file_path: Path,
+) -> list[str]:
+    with open(file_path) as file:
+        lines = [line.strip() for line in file]
+    return lines
+
+
+def parse_input(file_content: list[str]) -> Any:
+    return
+
+
+def solve_01(data: Any) -> int:
+    return
+
+
+def main() -> None:
+    # input.txt | example_1.txt
+    file_content = read_data(INPUT_FILE_PATH / "example_1.txt")
+    data = parse_input(file_content)
+    solution = solve_01(data)
+    print(f"The solution of the example 1 is {solution}")  # Solution 7
+    file_content = read_data(INPUT_FILE_PATH / "input.txt")
+    data = parse_input(file_content)
+    solution = solve_01(data)
+    print(f"The solution of the part 1 is {solution}")  # Solution
+
+
+if __name__ == "__main__":
+    main()
