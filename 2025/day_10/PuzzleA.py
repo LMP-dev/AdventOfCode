3bc5ba31f7a6c2ab8dffdcdf3ea9b2237b24e664
LMP-dev <Lmartorellester@gmail.com>
2025-12-14 20:01:55 +0100

    feat: solved part 1

--- 2025/day_10/PuzzleA.py
+++ 2025/day_10/PuzzleA.py
@@ -103,10 +103,10 @@ def main() -> None:
     data = parse_input(file_content)
     solution = solve_01(data)
     print(f"The solution of the example 1 is {solution}")  # Solution 7
-    # file_content = read_data(INPUT_FILE_PATH / "input.txt")
-    # data = parse_input(file_content)
-    # solution = solve_01(data)
-    # print(f"The solution of the part 1 is {solution}")  # Solution
+    file_content = read_data(INPUT_FILE_PATH / "input.txt")
+    data = parse_input(file_content)
+    solution = solve_01(data)
+    print(f"The solution of the part 1 is {solution}")  # Solution 434
 
 
 if __name__ == "__main__":
