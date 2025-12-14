5b49f2325a25a26ae04fde621bfac8d0e88241da
LMP-dev <Lmartorellester@gmail.com>
2025-12-14 13:35:29 +0100

    feat: solved part 1

--- 2025/day_09/PuzzleA.py
+++ 2025/day_09/PuzzleA.py
@@ -17,23 +17,31 @@ def parse_input(file_content: list[str]) -> list[tuple[int, int]]:
     return [tuple(map(int, line.split(","))) for line in file_content]
 
 
+def calculate_area(
+    pair: tuple[tuple[int, int], tuple[int, int]],
+) -> int:
+    p1, p2 = pair
+    side_a = abs(p1[0] - p2[0]) + 1
+    side_b = abs(p1[1] - p2[1]) + 1
+    return side_a * side_b
+
+
 def solve_01(data: list[tuple[int, int]]) -> int:
+    # Calculate all areas between rectangles
+    pairs_of_points = list(combinations(data, 2))
+    areas = {pair: calculate_area(pair) for pair in pairs_of_points}
 
-    # take first point and calculate rest of diagonals to other points
-    # sort highest to lowest diagonals
-    #     side a = abs(x1-x2)+1
-    #     side b = abs(y1-y2)+1
-    #     diagonal = math.sqrt(a**2+b**2)
+    # Sort by areas from high to low
+    sorted_areas = {
+        k: v for k, v in sorted(areas.items(), key=lambda item: item[1], reverse=True)
+    }
 
-    # Calculate real area highest diagonal
-    #     A = a*b
-    # Check only rest of diagonals that d >= math.sqrt(2*A)  # maximum area diagonal have is the square
-    # Find largest rectange
+    biggest_rectangle = list(sorted_areas.keys())[0]
+    biggest_area = list(sorted_areas.values())[0]
 
-    # Repeat for the other combinations of points
-    # Directly consider only diagonals that d >= math.sqrt(2*A)
+    print(f"the bissgest rectangle is formed by points: {biggest_rectangle}")
 
-    return
+    return biggest_area
 
 
 def main() -> None:
@@ -45,7 +53,7 @@ def main() -> None:
     file_content = read_data(INPUT_FILE_PATH / "input.txt")
     data = parse_input(file_content)
     solution = solve_01(data)
-    print(f"The solution of the part 1 is {solution}")
+    print(f"The solution of the part 1 is {solution}")  # Solution 4776487744
 
 
 if __name__ == "__main__":
